a7de5def60c702795b8e4474c6394dce0dbfa8d4
retreat1188 <reelaw456@gmail.com>
2021-11-09 16:47:51 -0800

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.kt
@@ -0,0 +1,62 @@
+package com.bignerdranch.android.criminalintent
+
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.os.Bundle
+import android.widget.DatePicker
+import androidx.core.content.ContentProviderCompat.requireContext
+import android.text.format.DateFormat
+import androidx.fragment.app.DialogFragment
+import java.lang.String.format
+import java.util.*
+import java.util.Calendar.*
+import java.util.Calendar.getInstance
+
+private const val ARG_DATE = "date"
+
+class DatePickerFragment : DialogFragment() {
+    interface Callbacks {
+        fun onDateSelected(date: Date)
+    }
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
+        val date = arguments?.getSerializable(ARG_DATE) as Date
+        val calendar = getInstance()
+        calendar.time = date
+        val calendarHour = calendar.get(HOUR_OF_DAY)
+        val calendarMinute = calendar.get(MINUTE)
+
+        val dateListener = DatePickerDialog.OnDateSetListener {
+            _: DatePicker, year: Int, month: Int, day: Int ->
+            val resultDate : Date =
+                    GregorianCalendar(year, month, day, calendarHour, calendarMinute).time
+
+            targetFragment?.let { fragment ->
+                (fragment as Callbacks).onDateSelected(resultDate)
+            }
+        }
+
+        val initialYear = calendar.get(YEAR)
+        val initialMonth = calendar.get(MONTH)
+        val initialDay = calendar.get(DAY_OF_MONTH)
+
+        return DatePickerDialog(
+                requireContext(),
+                dateListener,
+                initialYear,
+                initialMonth,
+                initialDay
+        )
+    }
+
+    companion object {
+        fun newInstance(date: Date): DatePickerFragment {
+            val args = Bundle().apply {
+                putSerializable(ARG_DATE, date)
+            }
+            return DatePickerFragment().apply {
+                arguments = args
+            }
+        }
+    }
+}
